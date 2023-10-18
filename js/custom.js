0b417f097b362eddeaedeceea9a3f17be08773ff
Najmul Hosen <113312192+Najmulhosen@users.noreply.github.com>
2023-10-19 02:58:49 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/*!
+  *Item: Portfolio Website
+  * Description: Personal Portfolio
+  * Author/Developer: Najmul Hosen
+
+/*----------- CUSTOM JS SCRIPTS -----------*/
+
+(function ($) {
+  "use strict";
+  $(function () {
+    // Code here executes When the DOM is loaded...
+  });
+  $(window).on("load", function () {
+    // Code here executes When the page is loaded
+  });
+})(jQuery);
